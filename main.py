9cc3507d93bccf0e3a5bcb7839b555b3ab926ce0
Даниил Луценко <110895420+DefaultDari@users.noreply.github.com>
2024-11-18 16:03:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ text= filename.read() #Чтение содержимого файла
 print(text) #Вывод самого текста
 word_count = len(text.split()) #Подсчет количества слов
 print(f"Количество слов в файле : {word_count}") #Вывод кол-ва слов
+filename.close() #Закрытие файла
